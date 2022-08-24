0ff48020a6c2fa356b0665152741816f5ffe7700
Alejandro Ponce <alexponce36@hotmail.com>
2022-08-23 20:51:09 -0500

    feat: validaciones en jsx

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDom from 'react-dom/client';
+import React from "react";
+import ReactDom from "react-dom/client";
 
-const root = ReactDom.createRoot(document.getElementById('root'));
+const root = ReactDom.createRoot(document.getElementById("root"));
 
 function Greeting() {
-    return <div>
-        <h1>Esto es un componente</h1>
-        <p>Lorem 123</p>
-    </div>;
+  const married = true;
+
+  return <h1>{married ? "Estoy casado! 😀" : "No estoy casado!"}</h1>;
+
+  if (married) {
+    return <h1>Estoy casado!</h1>;
+  } else {
+    return <h1>No estoy casado!</h1>;
+  }
 }
 
-root.render(<div>
-    <Greeting></Greeting>
-    <Greeting/>
-</div>);
-
+root.render(<Greeting />);
