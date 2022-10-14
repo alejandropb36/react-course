e7fc4237d80c108896f8e70254a0234e406c685d
Alejandro Ponce <alexponce36@hotmail.com>
2022-10-13 20:18:48 -0500

    feat: class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { TaskCard } from "./Task";
+import { Saludar } from "./Saludar";
 
 export function App() {
   return (
     <>
       <TaskCard ready={false} />
-      
+      <Saludar/>
     </> // This is React Fragment
   );
 }
---
+++ src/Saludar.jsx
@@ -0,0 +1,10 @@
+import { Component } from 'react'
+
+
+export class Saludar extends Component {
+    render () {
+        return (
+            <h1>Hello world</h1>
+        );
+    }
+}
