9576a744350c8c8184c7f7415f3001b6ce0729f2
Alejandro Ponce <alexponce36@hotmail.com>
2022-11-03 20:33:30 -0600

    feat: use effect

--- src/From.jsx
+++ src/From.jsx
@@ -1,19 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export function Form() {
   const [mensaje, setMensaje] = useState("");
+  const [counter, setCounter] = useState(0);
 
-  console.log(mensaje);
+  useEffect(() => {
+    console.log("render");
+  }, [counter]);
 
   return (
     <>
       <div>
         <input type="text" onChange={(e) => setMensaje(e.target.value)} />
 
-        <button onClick={() => {
+        <button
+          onClick={() => {
             alert("El mensaje es: " + mensaje);
-        }}>Save</button>
+          }}
+        >
+          Save
+        </button>
       </div>
+
+      <hr />
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => {
+        setCounter(prevCounter => prevCounter + 1)
+      }}>Incrementar</button>
     </>
   );
 }
